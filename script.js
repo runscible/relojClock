5d6f81e68eb22abe2b12974f2da6175710b5f4cf
runscible <runscible@gmail.com>
2016-05-10 01:40:57 -0300

    new branch

--- script.js
+++ script.js
@@ -5,31 +5,32 @@ $(document).ready( function() {
   function displayTime() {
     var currentTime = new Date();
     var gmt = 4;
+    
 		
 
     var hours = currentTime.getHours();
     var minutes = currentTime.getMinutes();
     var seconds = currentTime.getSeconds();
+    var globalHours = hours+gmt;
 
 
-    if(hours < 10) {
-      hours = "0" + hours;
+    if(globalHours < 10) {
+      globalHours = "0" + globalHours;
     }
-	if (hours > 24){
+
 	
-	hours = hours-24;
-	}
     if(minutes < 10) {
       minutes = "0" + minutes;
     }        
     if(seconds < 10) {
        seconds = "0" + seconds;
     }
-
+	
 
     var clockDivLondon = document.getElementById('clock');
 
-    clockDivLondon.innerText = hours +gmt+ ":" + minutes + ":" + seconds;
+    clockDivLondon.innerText = globalHours +":" + minutes + ":" + seconds;
+	
 
 
 
@@ -42,17 +43,32 @@ $(document).ready( function() {
 
    //San Francisco
    var gmt = -4;
+   var globalHours = hours+gmt;
+	if(globalHours<0){
+	  globalHours = globalHours+24;
+	} 
+
+
+	 
+	
    var clockDivSanFrancisco = document.getElementById('clockSanFrancisco');
-   clockDivSanFrancisco.innerText = hours +gmt+ ":" + minutes + ":" + seconds;
+   clockDivSanFrancisco.innerText = globalHours + ":" + minutes + ":" + seconds;
    
 
    //Moscow	
    var gmt = 6;
+   var globalHours = hours+gmt;
+		
+	if(globalHours>24){
+	globalHours = globalHours-24;
+	}	
+
    var clockDivMoscow = document.getElementById('clockMoscow');
-   clockDivMoscow.innerText = hours +gmt+ ":" + minutes + ":" + seconds;
+   clockDivMoscow.innerText = globalHours+ ":" + minutes + ":" + seconds;
    	
   }
-
+   
+    
     
   
 	
